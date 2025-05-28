aba9f78c827177bd06536550af5f2b55e52cd4c2
AdapaGreeshmiKarunya <adapagreeshmikarunya@gmail.com>
2025-05-28 15:16:26 +0530

    patterns

---
+++ Day-2/Patterns/PatterJ.java
@@ -0,0 +1,17 @@
+public class PatterJ {
+    public static void main(String[] args) {
+        for(int row=0;row<7;row++)
+        {
+            for(int col=0;col<5;col++)
+            {
+                if(col==0 && row!=1 && row!=2 && row!=3 && row!=4 || col==1 && row!=1 && row!=2 && row!=3 && row!=4 && row!=5 || col==2 ||col==3 && row!=1 && row!=2 && row!=3 && row!=4 && row!=5 && row!=6 || col==4 && row!=1 && row!=2 && row!=3 && row!=4 && row!=5 && row!=6){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day-2/Patterns/PatternK.java
@@ -0,0 +1,17 @@
+public class PatternK {
+    public static void main(String[] args) {
+        for(int row=0;row<7;row++)
+        {
+            for(int col=0;col<5;col++)
+            {
+                if(col==0 ||row+col==4||row-col==2){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
