25bf2f3d6ceb3df29da494eb9bd7f330b4c7dbad
AdapaGreeshmiKarunya <adapagreeshmikarunya@gmail.com>
2025-05-29 12:31:37 +0530

    Patterns

---
+++ Day-3/Star Pattern/Pattern10.java
@@ -0,0 +1,18 @@
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int row=1;row<=n;row++){
+            for(int space=1;space<=n-row;space++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=2*row-1;col++){
+                if(col==1||col==2*row-1||row==5){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Day-3/Star Pattern/Pattern9.java
@@ -0,0 +1,26 @@
+public class Pattern9 {
+    public static void main(String[] args) {
+        int n=5;
+        for(int row=0;row<n;row++)
+        {
+            for(int space=1;space<=row;space++)
+             {
+                System.out.print(" ");
+             }
+            for(int col=1;col<=n-row;col++){
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int row=2;row<=n;row++){
+            for(int space=1;space<=n-row;space++){
+                System.out.print(" ");
+            }
+            for(int col=1;col<=row;col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
