4bee69ee005fd56f1dfd0b6ab6136f337c86812d
AdapaGreeshmiKarunya <adapagreeshmikarunya@gmail.com>
2025-06-02 10:12:45 +0530

    program

--- Day-6/PowerOfTwo.java
+++ Day-6/PowerOfTwo.java
@@ -12,10 +12,12 @@ public class PowerOfTwo {
         }
     }
     public static boolean isPowerOfTwo(int n) {
-        if (n <= 0) return false;
+        // Alternative method using bit manipulation
+        /*if (n <= 0) return false;
         while (n % 2 == 0) {
             n = n / 2;
         }
-        return n == 1;
+        return n == 1;*/
+        return n > 0 && (n & (n - 1)) == 0; // Efficient bit manipulation method
     }
 }
